6f6eeeff9e6095a65b3bfddfdda8aa86013f8a6c
sk-pathan <shoaibkp313@gmail.com>
2023-11-03 10:48:34 +0530

    I have make a clone of award wining website

---
+++ game.js
@@ -0,0 +1,83 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+const order=gsap.timeline()
+
+order.to("#logo svg,.menu",{
+    y:-90,
+    duration:.3,
+    opacity:0,
+    scrollTrigger:{
+        trigger:".center",
+        scroller:"#main",
+        delay:0.3,
+        start:"top 0",
+        end:"top -5%",
+        scrub:1
+        
+
+    }
+})
+order.to("#logo img",{
+    y:-90,
+    duration:0.3,
+    scrollTrigger:{
+        trigger:".center",
+        scroller:"#main",
+        start:"top 0",
+        end:"top -1%",
+        scrub:1
+        
+
+    }
+})
+order.from(" .shopbox",{
+    y:-120,
+    opacity:0,
+    stagger:0.3,
+    scrollTrigger:{
+        trigger:".page3",
+        scroller:"#main",
+        start:"top 50%",
+        end:"top 0%",
+        scrub:7
+    }
+})
+
+window.addEventListener("load",()=>{
+    gsap.from(" .center h1",{
+        y:-120,
+        opacity:0,
+        stagger:0.3,
+    })
+})
+
+
+
+
